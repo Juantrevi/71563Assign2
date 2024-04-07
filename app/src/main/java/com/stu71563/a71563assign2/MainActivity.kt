d53e2454afbeeb4715f72352f78299e5e78817cf
Juantrevi <juantrevi70@gmail.com>
2024-04-07 11:35:06 +0100

    -- Final Version --

--- app/src/main/java/com/stu71563/a71563assign2/MainActivity.kt
+++ app/src/main/java/com/stu71563/a71563assign2/MainActivity.kt
@@ -21,14 +21,3 @@ class MainActivity : ComponentActivity() {
     }
 }
 }
-
-/*{
-    "name": "String",
-    "image": "String",
-    "certification": "String",
-    "description": "String",
-    "starring": "String Array",
-    "running_time_mins": "Int",
-    "seats_remaining": "Int",
-    "seats_selected": "Int"
-}*/
